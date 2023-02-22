dded9907c607cc2ed56ceae962b89f7eff874345
jmoyes99 <j.m.moyes@student.saintkentigern.com>
2023-02-23 09:03:17 +1300

    Productivity tool

---
+++ Productivity.py
@@ -0,0 +1,69 @@
+# This is a code to offend people who want to learn how to get up and have a productive day
+
+
+from time import sleep
+
+print("What do you want")
+sleep(1)
+help = input("""Help or no?:
+""")
+help = help.strip().lower()
+
+help_yes = False
+normal_age = False
+is_awake = False
+
+if help == 'help' or help == 'yes':
+    print("Fine. I'll help you do one of the simplest things someone can do.")
+    sleep(2)
+    print("""
+
+
+
+
+
+""")
+    print("I'm calling you stupid.")
+    sleep(1)
+    help_yes = True
+elif help == 'no':
+    print("Okay, then why'd you ask genius?")
+else:
+    print("That isn't a 'yes' or 'no' so you dumb")
+
+if help_yes == True:
+    name = str(input("""What's your name?
+"""))
+
+    name = name.strip().lower().capitalize()
+
+if name == 'Jaquavius':
+    print("That's an awesome name")
+
+elif name != 'Jaquavius':
+    print(name + " is a dumb name. You should change it to something like Jaquavius.")
+
+age = int(input("How old are you? "))
+
+if age > 100:
+    print("You dead boi. Can't type if you're dead so go away.")
+elif age <= 0:
+    print("tf how you typing.")
+elif age >0<5:
+    print("Little toddler. Go away.")
+elif age >5<100:
+    print("Ok you normal age")
+    normal_age = True
+else:
+    print("What even is that. That not number")
+
+if normal_age = True
+    print("Let's get started.")
+    sleep(1)
+    awake = input("Are you awake?")
+
+awake = awake.strip().lower()
+
+if awake == 'yes':
+    print("Well that's good to hear.")
+    set is_awake
